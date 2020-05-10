93294e62b556990e09b6857bc5a618aab9daec0c
BorjaEst <boressan@outlook.com>
2020-05-10 15:05:54 +0200

    score handler and pool templates created

---
+++ src/score_pool.erl
@@ -0,0 +1,221 @@
+%%%-------------------------------------------------------------------
+%%% @author borja
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(score_pool).
+-compile([export_all, nowarn_export_all]). %% TODO: To delete after buil
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+-export_type([pool/0]).
+
+-type pool() :: term().
+
+%% gen_server callbacks
+-export([init/1, terminate/2, code_change/3]).
+-export([handle_call/3, handle_cast/2, handle_info/2]).
+
+-record(state, {}).
+
+-define(TAB_CONFIGUTATION, [
+    protected,    % Anyone can look but only the owner can write
+    ordered_set   % Ordered by score
+]).
+-define(INIT_SCORE, 0.0).
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link() ->
+    {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+    gen_server:start_link(?MODULE, [], []).
+
+
+
+
+%%--------------------------------------------------------------------
+%% @doc Returns the top N of an score pool in a format {Id, Score}.
+%% @end
+%%--------------------------------------------------------------------
+-spec top(pool(), N :: integer()) -> 
+    [{Score :: float(), Agent_Id :: agent:id()}].
+top(Pool, N) -> last_n(Pool, ets:last(Pool), N).
+
+last_n(_Pool, '$end_of_table',_N)       -> [];
+last_n( Pool,      ScoreAgent, N) when N > 0 ->
+    [ScoreAgent|last_n(Pool, ets:prev(Pool,ScoreAgent), N-1)];
+last_n(_Pool,     _ScoreAgent,_N)       -> [].
+
+%%--------------------------------------------------------------------
+%% @doc Returns the N agents with the lowest score.
+%% @end
+%%--------------------------------------------------------------------
+-spec bottom(pool(), N :: integer()) -> 
+    [{Score :: float(), Agent_Id :: agent:id()}].
+bottom(Pool, N) -> first_n(Pool, ets:first(Pool), N).
+
+first_n(_Pool, '$end_of_table',_N)            -> [];
+first_n( Pool,      ScoreAgent, N) when N > 0 ->
+    [ScoreAgent|first_n(Pool,ets:next(Pool,ScoreAgent), N-1)];
+first_n(_Pool,     _ScoreAgent,_N)            -> [].
+
+
+%%--------------------------------------------------------------------
+%% @doc Returns the N agents with the lowest score.
+%% @end
+%%--------------------------------------------------------------------
+-spec to_list(pool()) -> 
+    [{Score :: float(), Agent_Id :: agent:id()}].
+to_list(Pool) -> lists:reverse(ets:tab2list(Pool)).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+    {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term()} | ignore).
+init([]) ->
+    {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+        State :: #state{}) ->
+    {reply, Reply :: term(), NewState :: #state{}} |
+    {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+    {noreply, NewState :: #state{}} |
+    {noreply, NewState :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+    {stop, Reason :: term(), NewState :: #state{}}).
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_cast(Request :: term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}} |
+    {noreply, NewState :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term(), NewState :: #state{}}).
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}} |
+    {noreply, NewState :: #state{}, timeout() | hibernate} |
+    {stop, Reason :: term(), NewState :: #state{}}).
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+        State :: #state{}) -> term()).
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
+        Extra :: term()) ->
+    {ok, NewState :: #state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
+%%====================================================================
+%% Eunit white box tests
+%%====================================================================
+
+% --------------------------------------------------------------------
+% TESTS DESCRIPTIONS -------------------------------------------------
+
+% --------------------------------------------------------------------
+% SPECIFIC SETUP FUNCTIONS -------------------------------------------
+
+% --------------------------------------------------------------------
+% ACTUAL TESTS -------------------------------------------------------
+
+% --------------------------------------------------------------------
+% SPECIFIC HELPER FUNCTIONS ------------------------------------------
+
--- src/scorer_sup.erl
+++ src/scorer_sup.erl
@@ -2,19 +2,83 @@
 %% @doc scorer top level supervisor.
 %% @end
 %%%-------------------------------------------------------------------
-
 -module(scorer_sup).
-
 -behaviour(supervisor).
 
 -export([start_link/0]).
-
+-export([start_group/0, stop_group/1]).
+-export([ start_pool/0,  stop_pool/1]).
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
+-define(SPECS_SCORE_SERVER(Id), #{
+    id       => Id,
+    start    => {gen_event, start_link, []},
+    restart  => permanent,
+    shutdown => 500,
+    modules  => [gen_event]
+}).
+
+-define(SPECS_SCORE_POOL(Id), #{
+    id       => Id,
+    start    => {score_pool, start_link, []},
+    restart  => permanent,
+    shutdown => 500,
+    modules  => [gen_server]
+}).
+
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% @doc Starts the supervisor.
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%--------------------------------------------------------------------
+%% @doc Starts a score server (group) under the supervisor. 
+%% @end
+%%--------------------------------------------------------------------
+start_group() ->
+    Id = make_ref(),
+    case supervisor:start_child(?SERVER, ?SPECS_SCORE_SERVER(Id)) of 
+        {ok, Pid} -> {ok, {Id, Pid}};
+        Other     -> Other
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Starts a score server (group) under the supervisor. 
+%% @end
+%%--------------------------------------------------------------------
+stop_group({Id, _}) ->
+    supervisor:terminate_child(?SERVER, Id).
+
+%%--------------------------------------------------------------------
+%% @doc Starts a score server (group) under the supervisor. 
+%% @end
+%%--------------------------------------------------------------------
+start_pool() ->
+    Id = make_ref(),
+    case supervisor:start_child(?SERVER, ?SPECS_SCORE_POOL(Id)) of 
+        {ok, Pid} -> {ok, {Id, Pid}};
+        Other     -> Other
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Starts a score server (group) under the supervisor. 
+%% @end
+%%--------------------------------------------------------------------
+stop_pool({Id, _}) ->
+    supervisor:terminate_child(?SERVER, Id).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
 
 %% sup_flags() = #{strategy => strategy(),         % optional
 %%                 intensity => non_neg_integer(), % optional
@@ -32,4 +96,8 @@ init([]) ->
     ChildSpecs = [],
     {ok, {SupFlags, ChildSpecs}}.
 
-%% internal functions
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
