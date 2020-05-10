95d81b4470012e11df893f74430284992df3ded4
BorjaEst <boressan@outlook.com>
2020-05-10 13:12:33 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+scorer
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/scorer_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc scorer public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(scorer_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    scorer_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
